968b364b2567810ed39743744356f604351a8be2
nazrin_PC <nazrinoi.work@gmail.com>
2025-04-25 06:12:40 +0800

    Added OOPS EX 2 and 3

--- functions.py
+++ functions.py
@@ -5,7 +5,21 @@ def salutations(name):
 def tambah (a,b):
     print (a + b )
 
+def GreetUser(username):
+    print(f"Hello, {username}! Welcome to the Python course.")
+
 # Calling the functions
 
-salutations("Nazrin")
-tambah(2,.3)
+salutations("Nazrin")# 1st function
+
+tambah(2,.3)# 2nd function
+
+GreetUser("John")# 3rd function
+
+
+def CalculateAverage(*args):
+    numbers=[args]
+    average=sum(args)/len(args)
+    print(f"The average of {args} are {average}")
+
+CalculateAverage(1,10,23,222)
